98e5ff3d99a41094a61bf3a35b9c5a2fce0edd12
ernaniraffo <ernani.raffo@icloud.com>
2023-03-15 21:01:25 -0700

    daily leetcode

---
+++ sumTreeNumbers.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        return postOrderTraversal(root, sum); 
+    }
+
+    int postOrderTraversal(TreeNode* root, int sum) {
+        if (root == nullptr) {
+            return 0;
+        }
+        sum = sum * 10 + root->val;
+        if (root->left == nullptr && root->right == nullptr) {
+            return sum;
+        }
+        int leftSum = postOrderTraversal(root->left, sum);
+        int rightSum = postOrderTraversal(root->right, sum);
+        return leftSum + rightSum;
+    }
+};
