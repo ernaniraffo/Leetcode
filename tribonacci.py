973d5e67f3e46fc7ba6dc5a876327689438a8144
ernaniraffo <ernani.raffo@icloud.com>
2023-03-18 00:32:39 -0700

    old one

---
+++ tribonacci.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        max_n = 38
+        F = [0] * max_n
+        F[0] = 0
+        F[1], F[2] = 1, 1
+        if n < 3:
+            return F[n]
+        curr = 3
+        while curr <= n:
+            F[curr] = F[curr - 3] + F[curr - 2] + F[curr - 1]
+            curr += 1
+        return F[n]
