3c7d8250cf33331e88ecee995b11225e2f3e63cb
ernaniraffo <ernani.raffo@icloud.com>
2023-03-18 23:23:09 -0700

    daily challenge

---
+++ wordDictionary.py
@@ -0,0 +1,34 @@
+class WordNode:
+
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+    def search(self, word: str) -> bool:
+        node = self
+        for i in range(len(word)):
+            if word[i] == '.':
+                for child in node.children.values():
+                    if child.search(word[i+1:]):
+                        return True
+                return False
+            if word[i] not in node.children:
+                return False
+            node = node.children[word[i]]
+        return node.end
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = WordNode() 
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = WordNode()
+            node = node.children[c]
+        node.end = True
+
+    def search(self, word: str) -> bool:
+        return self.root.search(word)
