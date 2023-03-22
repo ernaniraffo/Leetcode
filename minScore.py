bce5f4e3134a0c3fec2f4621eb689f8abde2471b
ernaniraffo <ernani.raffo@icloud.com>
2023-03-21 21:28:37 -0700

    c++ solution faster but its ok

---
+++ minScore.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for road in roads:
+            adj[road[0]].append((road[1], road[2]))
+            adj[road[1]].append((road[0], road[2]))
+        visited = {1}
+        q = [1]
+        score = sys.maxsize
+        while q:
+            n = q.pop(0)
+            for node, distance in adj[n]:
+                score = min(score, distance)
+                if node not in visited:
+                    visited.add(node)
+                    q.append(node)
+        return score
+
