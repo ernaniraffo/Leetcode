35ae771949df491ecc0ecda1ec9f314049951dcc
ernaniraffo <ernani.raffo@icloud.com>
2023-06-17 16:20:41 -0700

    easy problem but could be better

---
+++ lengthOfLastWord.c
@@ -0,0 +1,13 @@
+int lengthOfLastWord(char * s){
+    int len = 0;
+    bool matched = false;
+    for (int c = strlen(s) - 1; c >= 0; c -= 1) {
+        if (s[c] == ' ' && matched) {
+            break;
+        } else if (s[c] != ' ') {
+            matched = true;
+            len += 1;
+        }
+    }
+    return len;
+}
