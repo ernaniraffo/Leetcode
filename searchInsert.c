8384bd781a95360d8ca9ccce0ffbb064a03be4eb
ernaniraffo <ernani.raffo@icloud.com>
2023-06-17 16:02:29 -0700

    binary search in C

---
+++ searchInsert.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target){
+    int l = 0;
+    int r = numsSize - 1;
+    int mid;
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (nums[mid] < target) {
+            l = mid + 1;
+        } else if (nums[mid] > target) {
+            r = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return l;
+}
