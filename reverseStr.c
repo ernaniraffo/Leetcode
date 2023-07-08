41bc1e519635c78be4661179109d1eb543835430
ernaniraffo <ernani.raffo@icloud.com>
2023-07-07 22:38:22 -0700

    best language + best bitwise operator

---
+++ reverseStr.c
@@ -0,0 +1,12 @@
+void reverseString(char* s, int sSize){
+    char* end = s + (sSize - 1);
+    if (s) {
+        while (s < end) {
+            *s ^= *end;
+            *end ^= *s;
+            *s ^= *end;
+            s++;
+            end--;
+        }
+    }
+}
