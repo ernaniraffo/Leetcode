060bc73cfb19d80708354e17dd0a7a30ef9acdfa
ernaniraffo <ernani.raffo@icloud.com>
2023-12-21 18:39:28 -0300

    old one, forgot to push

---
+++ removeDuplicates.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        seen = set()
+        while (i < len(nums)):
+            if nums[i] in seen:
+                nums.pop(i)
+            else:
+                seen.add(nums[i])
+                i += 1
+        return i
