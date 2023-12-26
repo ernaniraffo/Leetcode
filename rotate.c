b98ae0add7a4187f9d9bb8d0dac2f4497be762c1
ernaniraffo <ernani.raffo@icloud.com>
2023-12-26 12:02:39 -0300

    rotate array, O(1) space

---
+++ rotate.c
@@ -0,0 +1,21 @@
+void rotate(int* nums, int numsSize, int k) {
+    int count = 0;
+    for (int start = 0; count < numsSize; start += 1) {
+        int curr = start;
+        int prev = nums[curr];
+        while (1) {
+            int next = (curr + k) % numsSize;
+            
+            int temp = nums[next];
+            nums[next] = prev;
+            prev = temp;
+            
+            count += 1;
+            curr = next;
+
+            if (curr == start) {
+                break;
+            }
+        }
+    }
+}
