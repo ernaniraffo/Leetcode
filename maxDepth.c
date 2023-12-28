cdf2a515bf1c3bf5c0068066fb8598b80536c7ba
ernaniraffo <ernani.raffo@icloud.com>
2023-12-28 20:24:19 -0300

    recursion

---
+++ maxDepth.c
@@ -0,0 +1,16 @@
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int maxDepth(struct TreeNode* root) {
+    if (root) {
+        int depth = 1;
+        int l = depth + maxDepth(root->left);
+        int r = depth + maxDepth(root->right);
+        return l > r ? l : r;
+    }
+    return 0;
+}
