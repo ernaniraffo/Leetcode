6ebd01f5a9dac914106b109e91ead50fd5288c59
ernaniraffo <ernani.raffo@icloud.com>
2024-01-13 16:03:52 -0800

    C version

---
+++ isSameTree.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p && q) {
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+    return !p && !q;
+}
