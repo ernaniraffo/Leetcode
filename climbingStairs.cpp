84270ab8ff5c0a248df8c9b17ae021cd029c4299
Ernani Raffo <ernani.raffo@icloud.com>
2024-07-27 19:09:43 -0700

    memoization

---
+++ climbingStairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        std::vector<int> memo(n + 1, 0);
+        return climbStairs(0, n, memo);
+    }
+    
+    int climbStairs(int step, int n, std::vector<int>& memo) {
+        if (step > n) {
+            return 0;
+        }
+        if (step == n) {
+            return 1;
+        }
+        if (memo[step] > 0) {
+            return memo[step];
+        }
+        memo[step] = climbStairs(step + 1, n, memo) + climbStairs(step + 2, n, memo);
+        return memo[step];
+    }
+};
